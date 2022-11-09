8c91d5835ccf5f876bd892d5281a88714b7b819d
Peter Hanecak <peter.hanecak@maptiler.com>
2022-11-09 22:12:51 +0100

    added benchmark (as disabled unit test) to time performance of Building.process()

--- src/test/java/org/openmaptiles/layers/BuildingTest.java
+++ src/test/java/org/openmaptiles/layers/BuildingTest.java
@@ -1,8 +1,10 @@
 package org.openmaptiles.layers;
 
+import static com.onthegomap.planetiler.TestUtils.newPoint;
 import static com.onthegomap.planetiler.TestUtils.rectangle;
 
 import com.onthegomap.planetiler.VectorTile;
+import com.onthegomap.planetiler.config.PlanetilerConfig;
 import com.onthegomap.planetiler.geo.GeoUtils;
 import com.onthegomap.planetiler.geo.GeometryException;
 import com.onthegomap.planetiler.reader.SimpleFeature;
@@ -11,8 +13,10 @@ import com.onthegomap.planetiler.reader.osm.OsmReader;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openmaptiles.OpenMapTilesProfile;
+import org.openmaptiles.generated.Tables;
 
 class BuildingTest extends AbstractLayerTest {
 
@@ -175,5 +179,36 @@ class BuildingTest extends AbstractLayerTest {
       "building:building", "yes",
       "building:colour", "#ff0000"
     ))));
+  }
+
+  @Disabled
+  @Test
+  void coalesceFInBuildingProcessBenchmark() {
+    /* The point is to hit Building.process hard (hence @Disabled annotation), measure time and see whether transition
+     * from `coalesce()` to `coalesceF()` helps tpo achieve shorter time. In theory, it should since half of calls of
+     * `parseDoubleOrNull()` should be avoided if firs value (say `height`) is not null thus lambda for second value
+     * (`parseDoubleOrNull` for say `buildingheight`) is not called.
+     *
+     * Values for this UT:
+     * a) before (with `coalesce()`): 3.690s, 3.638s, 3.577s
+     * b) after (with `coalesceF()`:  2.564s, 2.436s, 2.458s
+     *
+     * Values for Slovakia tiles generation:
+     * a) before: Finished in 5m40s cpu:28m38s gc:10s avg:5.1; ... 5m38s cpu:28m52s gc:9s avg:5.1; ... 5m30s cpu:28m16s gc:9s avg:5.1
+     * b) after:  Finished in 4m53s cpu:28m3s gc:8s avg:5.7;   ... 4m45s cpu:27m11s gc:8s avg:5.7; ... 4m44s cpu:27m8s gc:8s avg:5.7
+     */
+    PlanetilerConfig config = PlanetilerConfig.defaults();
+    Building building = new Building(null, config, null);
+    var sourceFeature = SimpleFeature.create(newPoint(0, 0), Map.of("abc", "123"), "source", "source_layer", 99);
+    Tables.OsmBuildingPolygon element = new Tables.OsmBuildingPolygon(
+      null, null, null,
+      null, "3661", "3662",
+      null, null, "3663",
+      "3664", "3665", "3666", sourceFeature
+    );
+
+    for (long i = 0; i < 10_000_000; i++) {
+      building.process(element, null);
+    }
   }
 }

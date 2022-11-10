3a3523b9d9dff6e6854ebee43ca54782c7babf1b
Peter Hanecak <peter.hanecak@maptiler.com>
2022-11-10 09:18:39 +0100

    unit tests extended to cover other coalesceF() variants

--- src/test/java/org/openmaptiles/util/UtilsTest.java
+++ src/test/java/org/openmaptiles/util/UtilsTest.java
@@ -25,8 +25,8 @@ public class UtilsTest {
   }
 
   @Test
-  public void coalesce2L() {
-    Function<TestRecord, Double> r2d = (TestRecord s) -> {
+  public void coalesceF2() {
+    Function<TestRecord, Double> fb = (TestRecord s) -> {
       counter2++;
       return Double.valueOf(s.value());
     };
@@ -34,10 +34,111 @@ public class UtilsTest {
     TestRecord a = new TestRecord("1");
     TestRecord b = new TestRecord("2");
 
-    Assertions.assertEquals(1, Utils.coalesceF(Double.valueOf(a.value()), r2d, b));
+    Assertions.assertEquals(1, Utils.coalesceF(Double.valueOf(a.value()), fb, b));
     Assertions.assertEquals(0, counter2);
 
-    Assertions.assertEquals(2, Utils.coalesceF(null, r2d, b));
+    Assertions.assertEquals(2, Utils.coalesceF(null, fb, b));
     Assertions.assertEquals(1, counter2);
+  }
+
+  @Test
+  public void coalesceF3() {
+    Function<TestRecord, Double> fb = (TestRecord s) -> {
+      counter2++;
+      return s == null ? null : Double.valueOf(s.value());
+    };
+    Function<TestRecord, Double> fc = (TestRecord s) -> {
+      counter3++;
+      return Double.valueOf(s.value());
+    };
+
+    TestRecord a = new TestRecord("1");
+    TestRecord b = new TestRecord("2");
+    TestRecord c = new TestRecord("3");
+
+    Assertions.assertEquals(1, Utils.coalesceF(Double.valueOf(a.value()), fb, b, fc, c));
+    Assertions.assertEquals(0, counter2);
+    Assertions.assertEquals(0, counter3);
+
+    Assertions.assertEquals(2, Utils.coalesceF(null, fb, b, fc, c));
+    Assertions.assertEquals(1, counter2);
+    Assertions.assertEquals(0, counter3);
+
+    Assertions.assertEquals(3, Utils.coalesceF(null, fb, null, fc, c));
+    Assertions.assertEquals(2, counter2);
+    Assertions.assertEquals(1, counter3);
+  }
+
+  @Test
+  public void coalesceF6() {
+    Function<TestRecord, Double> fb = (TestRecord s) -> {
+      counter2++;
+      return s == null ? null : Double.valueOf(s.value());
+    };
+    Function<TestRecord, Double> fc = (TestRecord s) -> {
+      counter3++;
+      return s == null ? null : Double.valueOf(s.value());
+    };
+    Function<TestRecord, Double> fd = (TestRecord s) -> {
+      counter4++;
+      return s == null ? null : Double.valueOf(s.value());
+    };
+    Function<TestRecord, Double> fe = (TestRecord s) -> {
+      counter5++;
+      return s == null ? null : Double.valueOf(s.value());
+    };
+    Function<TestRecord, Double> ff = (TestRecord s) -> {
+      counter6++;
+      return s == null ? null : Double.valueOf(s.value());
+    };
+
+    TestRecord a = new TestRecord("1");
+    TestRecord b = new TestRecord("2");
+    TestRecord c = new TestRecord("3");
+    TestRecord d = new TestRecord("4");
+    TestRecord e = new TestRecord("5");
+    TestRecord f = new TestRecord("6");
+
+    Assertions.assertEquals(1, Utils.coalesceF(Double.valueOf(a.value()), fb, b, fc, c, fd, d, fe, e, ff, f));
+    Assertions.assertEquals(0, counter2);
+    Assertions.assertEquals(0, counter3);
+    Assertions.assertEquals(0, counter4);
+    Assertions.assertEquals(0, counter5);
+    Assertions.assertEquals(0, counter6);
+
+    Assertions.assertEquals(2, Utils.coalesceF(null, fb, b, fc, c, fd, d, fe, e, ff, f));
+    Assertions.assertEquals(1, counter2);
+    Assertions.assertEquals(0, counter3);
+    Assertions.assertEquals(0, counter4);
+    Assertions.assertEquals(0, counter5);
+    Assertions.assertEquals(0, counter6);
+
+    Assertions.assertEquals(3, Utils.coalesceF(null, fb, null, fc, c, fd, d, fe, e, ff, f));
+    Assertions.assertEquals(2, counter2);
+    Assertions.assertEquals(1, counter3);
+    Assertions.assertEquals(0, counter4);
+    Assertions.assertEquals(0, counter5);
+    Assertions.assertEquals(0, counter6);
+
+    Assertions.assertEquals(4, Utils.coalesceF(null, fb, null, fc, null, fd, d, fe, e, ff, f));
+    Assertions.assertEquals(3, counter2);
+    Assertions.assertEquals(2, counter3);
+    Assertions.assertEquals(1, counter4);
+    Assertions.assertEquals(0, counter5);
+    Assertions.assertEquals(0, counter6);
+
+    Assertions.assertEquals(5, Utils.coalesceF(null, fb, null, fc, null, fd, null, fe, e, ff, f));
+    Assertions.assertEquals(4, counter2);
+    Assertions.assertEquals(3, counter3);
+    Assertions.assertEquals(2, counter4);
+    Assertions.assertEquals(1, counter5);
+    Assertions.assertEquals(0, counter6);
+
+    Assertions.assertEquals(6, Utils.coalesceF(null, fb, null, fc, null, fd, null, fe, null, ff, f));
+    Assertions.assertEquals(5, counter2);
+    Assertions.assertEquals(4, counter3);
+    Assertions.assertEquals(3, counter4);
+    Assertions.assertEquals(2, counter5);
+    Assertions.assertEquals(1, counter6);
   }
 }

ed7e357d7d8cc4e44ceb41462f7b6423a65d8e5e
Peter Hanecak <peter.hanecak@maptiler.com>
2023-10-09 20:36:42 +0200

    Convert separated addresses to dashed addresses

--- src/test/java/org/openmaptiles/layers/HousenumberTest.java
+++ src/test/java/org/openmaptiles/layers/HousenumberTest.java
@@ -1,5 +1,7 @@
 package org.openmaptiles.layers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -26,5 +28,44 @@ class HousenumberTest extends AbstractLayerTest {
     )), process(polygonFeature(Map.of(
       "addr:housenumber", "10"
     ))));
+  }
+
+  @Test
+  void testDisplayHousenumberNonRange() {
+    final String HOUSENUMBER = "1";
+    assertEquals(HOUSENUMBER, Housenumber.displayHousenumber(HOUSENUMBER));
+  }
+
+  @Test
+  void testDisplayHousenumberNumericRange() {
+    final String HOUSENUMBER = "1;1a;2;2/b;20;3";
+    assertEquals("1–3", Housenumber.displayHousenumber(HOUSENUMBER));
+  }
+
+  @Test
+  void testDisplayHousenumberNumericRangeBroken() {
+    final String HOUSENUMBER = "1;1a;2;2/b;20;3;";
+    assertEquals("1–3", Housenumber.displayHousenumber(HOUSENUMBER));
+  }
+
+  @Test
+  void testDisplayHousenumberNonnumericRange() {
+    final String HOUSENUMBER = "1;2;20;3";
+    assertEquals("1–20", Housenumber.displayHousenumber(HOUSENUMBER));
+  }
+
+  @Test
+  void testDisplayHousenumberNonnumericRangeBroken() {
+    final String HOUSENUMBER = "1;2;20;3;";
+    assertEquals("1–20", Housenumber.displayHousenumber(HOUSENUMBER));
+  }
+
+  @Test
+  void testDisplayHousenumberExtraBroken() {
+    final String HOUSENUMBER_1 = ";";
+    assertEquals(HOUSENUMBER_1, Housenumber.displayHousenumber(HOUSENUMBER_1));
+
+    final String HOUSENUMBER_2 = ";;";
+    assertEquals(HOUSENUMBER_2, Housenumber.displayHousenumber(HOUSENUMBER_2));
   }
 }

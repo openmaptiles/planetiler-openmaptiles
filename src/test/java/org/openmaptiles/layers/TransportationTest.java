176354816f16504fc13d89a76c77b600ebda6932
Peter Hanecak <115141505+phanecak-maptiler@users.noreply.github.com>
2023-11-08 11:34:22 +0100

    fix missing name:xx for roads in transportation_name (#58) (#129)
    * fix missing name:xx for roads in transportation_name (#58)
    
    * mvn spotless:apply

--- src/test/java/org/openmaptiles/layers/TransportationTest.java
+++ src/test/java/org/openmaptiles/layers/TransportationTest.java
@@ -1357,4 +1357,30 @@ class TransportationTest extends AbstractLayerTest {
       "service", "driveway"
     ))));
   }
+
+  @Test
+  void testIssue58() {
+    // test subject: https://www.openstreetmap.org/way/222564359
+    // note: "name:es" used instead of "name:ar" since we've setup only "de" and "es" for unit tests
+    FeatureCollector result = process(lineFeature(Map.of(
+      "name", "איילון דרום",
+      "name:es", "أيالون جنوب",
+      "name:en", "Ayalon South",
+      "highway", "motorway"
+    )));
+    assertFeatures(4, List.of(Map.of(
+      "_layer", "transportation",
+      "_type", "line",
+      "class", "motorway"
+    ), Map.of(
+      "_layer", "transportation_name",
+      "_type", "line",
+      "class", "motorway",
+      "name", "איילון דרום",
+      "name_int", "Ayalon South",
+      "name:latin", "Ayalon South",
+      "name:es", "أيالون جنوب",
+      "name:en", "Ayalon South"
+    )), result);
+  }
 }

52ced49dcac7282ee0b55506cd0d84fb5c171cae
Michael Barry <msbarry@users.noreply.github.com>
2024-01-15 14:48:34 -0500

    Check for deterministic output in CI (#149)

--- scripts/test-release.sh
+++ scripts/test-release.sh
@@ -6,14 +6,18 @@ version="${1:-$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 echo "Test java build"
 echo "::group::OpenMapTiles monaco (java)"
-rm -f data/out.mbtiles
-java -jar target/*with-deps.jar --download --area=monaco --mbtiles=data/out.mbtiles
-./scripts/check-monaco.sh data/out.mbtiles
+# use different numbers of threads to stress-test determinism check
+java -jar target/*with-deps.jar --download --area=monaco --output=data/java.mbtiles --threads=4
+./scripts/check-monaco.sh data/java.mbtiles
 echo "::endgroup::"
 
 echo "::endgroup::"
 echo "::group::OpenMapTiles monaco (docker)"
-rm -f data/out.mbtiles
-docker run -v "$(pwd)/data":/data openmaptiles/planetiler-openmaptiles:"${version}" --area=monaco --mbtiles=data/out.mbtiles
-./scripts/check-monaco.sh data/out.mbtiles
+rm -f data/docker.mbtiles
+docker run -v "$(pwd)/data":/data openmaptiles/planetiler-openmaptiles:"${version}" --area=monaco --output=data/docker.mbtiles --threads=32
+./scripts/check-monaco.sh data/docker.mbtiles
+echo "::endgroup::"
+
+echo "::group::Compare"
+java -cp target/*with-deps.jar com.onthegomap.planetiler.util.CompareArchives data/java.mbtiles data/docker.mbtiles
 echo "::endgroup::"

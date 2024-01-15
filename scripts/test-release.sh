f822508f2c19f26522fbcdc78c9a8eb5fe556fdf
Mike Barry <msb5014@gmail.com>
2024-01-15 14:31:28 -0500

    vary threads

--- scripts/test-release.sh
+++ scripts/test-release.sh
@@ -6,14 +6,15 @@ version="${1:-$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 echo "Test java build"
 echo "::group::OpenMapTiles monaco (java)"
-java -jar target/*with-deps.jar --download --area=monaco --mbtiles=data/java.mbtiles
+# use different numbers of threads to stress-test determinism check
+java -jar target/*with-deps.jar --download --area=monaco --output=data/java.mbtiles --threads=4
 ./scripts/check-monaco.sh data/java.mbtiles
 echo "::endgroup::"
 
 echo "::endgroup::"
 echo "::group::OpenMapTiles monaco (docker)"
 rm -f data/docker.mbtiles
-docker run -v "$(pwd)/data":/data openmaptiles/planetiler-openmaptiles:"${version}" --area=monaco --mbtiles=data/docker.mbtiles
+docker run -v "$(pwd)/data":/data openmaptiles/planetiler-openmaptiles:"${version}" --area=monaco --output=data/docker.mbtiles --threads=32
 ./scripts/check-monaco.sh data/docker.mbtiles
 echo "::endgroup::"
 

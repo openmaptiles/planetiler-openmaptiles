7d273de92f7000fbf27cf5d37af051ebcd361661
Mike Barry <msb5014@gmail.com>
2026-01-16 05:50:57 -0500

    format

--- src/main/java/org/openmaptiles/layers/TransportationName.java
+++ src/main/java/org/openmaptiles/layers/TransportationName.java
@@ -248,7 +248,7 @@ public class TransportationName implements
       isLink ? 13 : 12; // fallback - get from line minzoom, but floor at 12
 
     // inherit min zoom threshold from visible road, and ensure we never show a label on a road that's not visible yet.
-    minzoom = Math.max(minzoom, transportation.getMinzoom(element, highwayClass));
+    minzoom = Math.max(minzoom, transportation.getMinzoomAndClass(element, highwayClass).minzoom());
     if (minzoom > config.maxzoom()) {
       return;
     }
